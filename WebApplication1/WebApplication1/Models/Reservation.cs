a3f6b622716f42aa2dad5500a41a8700587ccb70
Szymon Bak <s34554@pjwskt.edu.pl>
2026-04-13 18:19:31 +0200

    Add data annotation and validation

--- WebApplication1/WebApplication1/Models/Reservation.cs
+++ WebApplication1/WebApplication1/Models/Reservation.cs
@@ -1,4 +1,5 @@
-﻿using WebApplication1.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using WebApplication1.Enums;
 
 namespace WebApplication1.Models;
 
@@ -6,9 +7,18 @@ public class Reservation
 {
     public int Id { get; set; }
     public int RoomId { get; set; }
+    [Required]
     public string OrganizerName { get; set; }
+    [Required]
+    public string Topic { get; set; }
     public DateOnly Date { get; set; }
     public TimeOnly StartTime { get; set; }
     public TimeOnly EndTime { get; set; }
     public ReservationStatus Status { get; set; }
+    
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (EndTime <= StartTime)
+            yield return new ValidationResult("EndTime must be later than StartTime", [nameof(EndTime)]);
+    }
 }
--- WebApplication1/WebApplication1/Models/Room.cs
+++ WebApplication1/WebApplication1/Models/Room.cs
@@ -1,11 +1,16 @@
-﻿namespace WebApplication1.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models;
 
 public class Room
 {
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; }
+    [Required]
     public int BuildingCode { get; set; }
     public int Floor { get; set; }
+    [Range(1, int.MaxValue, ErrorMessage = "Capacity must be greater than zero")]
     public int Capacity { get; set; }
     public bool HasProjector { get; set; }
     public bool IsActive { get; set; }
